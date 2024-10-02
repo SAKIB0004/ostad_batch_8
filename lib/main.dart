ce2d1a91e9892e44aff40c33b2e6fc08a571f17d
Ostad_B_8 <mahmudulhaque540@gmail.com>
2024-10-02 14:05:33 +0600

    changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,37 +1,44 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-void main(){
+//reformat - ctl + shift + l
+//suggestion - ctl + space
+
+void main() {
   runApp(HelloWorldApp());
 }
 
-class HelloWorldApp extends StatelessWidget{
+class HelloWorldApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: "My Hello World App",
-      home: Scaffold(
-        body: Center(
-          child: Text(
-            "In 2009, Ronaldo transferred to Real Madrid for a then world record £80 million.[92] At least 80,000 fans attended his presentation at the Santiago Bernabéu, surpassing the 25-year record of 75,000 fans who had welcomed Diego Maradona at Napoli.[93]Ronaldo made his La Liga debut against Deportivo La Coruña on 29 August, scoring a penalty in a 3–2 home win.[94] He scored in each of his first four league games",
-            textAlign: TextAlign.justify,
-            maxLines: 3,
-            style: TextStyle(
-                fontSize: 32,
-                color: Colors.green,
-                fontWeight: FontWeight.bold,
-                fontStyle: FontStyle.italic,
-                height: 1.5,
-                letterSpacing: 10,
-                //backgroundColor: Colors.amber,
-                wordSpacing: 5,
-                overflow: TextOverflow.ellipsis
-                //decoration: TextDecoration.underline,
-          ),),
-        ),
-      )
-    );
+        debugShowCheckedModeBanner: false,
+        title: "My Hello World App",
+        home: Scaffold(
+          backgroundColor: Colors.amberAccent,
+          appBar: AppBar(
+            title: Text("Home"),
+            centerTitle: true,
+          ),
+          body: Center(
+            child: Text(
+              "In 2009, Ronaldo transferred to Real Madrid for a then world record £80 million.[92] At least 80,000 fans attended his presentation at the Santiago Bernabéu, surpassing the 25-year record of 75,000 fans who had welcomed Diego Maradona at Napoli.[93]Ronaldo made his La Liga debut against Deportivo La Coruña on 29 August, scoring a penalty in a 3–2 home win.[94] He scored in each of his first four league games",
+              textAlign: TextAlign.justify,
+              maxLines: 3,
+              style: TextStyle(
+                  fontSize: 32,
+                  color: Colors.green,
+                  fontWeight: FontWeight.bold,
+                  fontStyle: FontStyle.italic,
+                  height: 1.5,
+                  letterSpacing: 10,
+                  //backgroundColor: Colors.amber,
+                  wordSpacing: 5,
+                  overflow: TextOverflow.ellipsis
+                  //decoration: TextDecoration.underline,
+                  ),
+            ),
+          ),
+        ));
   }
-
 }
