84cbbea508914abb9d48ab3849faebc3e15a6e5c
Ostad_B_8 <mahmudulhaque540@gmail.com>
2024-10-08 22:14:42 +0600

    images-row-column

--- lib/main.dart
+++ lib/main.dart
@@ -34,7 +34,10 @@ class myApp extends StatelessWidget {
             alignment: Alignment.topRight,
         ),*/
 
-        body: Column(
+        /*body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisSize: MainAxisSize.min,
           children: [
             Text('heloo'),
             Text('world'),
@@ -42,16 +45,70 @@ class myApp extends StatelessWidget {
               'https://st3.depositphotos.com/1033600/19135/i/450/depositphotos_191352360-stock-illustration-wax-figure-of-loki-fictional.jpg',
               width: 200,
               height: 500,
-              //fit: BoxFit.fitHeight,
-              alignment: Alignment.topRight,
+              fit: BoxFit.fitHeight,
+              //alignment: Alignment.topRight,
             ),
-
+            Text('This is image'),
+            Text('Im Sakib'),
           ],
-        )
+        ),*/
 
+        /*body: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.end,
 
+          children: [
+            Text('helloo '),
+            Text('world '),
+            Image.network(
+              'https://st3.depositphotos.com/1033600/19135/i/450/depositphotos_191352360-stock-illustration-wax-figure-of-loki-fictional.jpg',
+              width: 200,
+              //height: 500,
+              fit: BoxFit.fitHeight,
+              //alignment: Alignment.topRight,
+            ),
+            Text('This is image '),
+            Text('Im Sakib '),
+          ],
+        ),*/
 
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text('Hello'),
+            Text('Sakib'),
+            Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text('Hello '),
+                  Text('Sakib '),
+                  Text('This is Row '),
+                ],
+            ),
+            Text('This is Column'),
+            Row(
+              children: [
+                Image.network(
+                  'https://st3.depositphotos.com/1033600/19135/i/450/depositphotos_191352360-stock-illustration-wax-figure-of-loki-fictional.jpg',
+                  width: 200,
+                  //height: 500,
+                  fit: BoxFit.fitHeight,
+                  //alignment: Alignment.topRight,
+                ),
+                Column(
+                  children: [
+                    Text('This is Loki'),
+                    Text('This is Loki'),
+                    Text('This is Loki'),
+                  ],
+                )
+              ],
+            ),
+          ],
 
+        ),
       ),
     );
   }
