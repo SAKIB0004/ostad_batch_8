29702b06dd40f6dfc999e393f1a7acec0deca69c
Ostad_B_8 <mahmudulhaque540@gmail.com>
2024-10-24 00:24:59 +0600

    Drawer, navigation bar, bottom navigation bar, listview,listview builder

--- lib/main.dart
+++ lib/main.dart
@@ -24,8 +24,50 @@ class home extends StatelessWidget{
       appBar: AppBar(
         title: Text("Home"),
         backgroundColor: Colors.amber,
-      ),
+        actions: [
+          IconButton(onPressed: (){}, icon: Icon(Icons.add)),
+          IconButton(onPressed: (){}, icon: Icon(Icons.account_circle)),
 
+        ],
+      ),
+      drawer: Drawer(
+        elevation: 0,
+        shadowColor: Colors.red,
+        width: 200,
+        child: Column(
+          children: [
+            Text("Random"),
+          ],
+        ),
+      ),
+      /*bottomNavigationBar: BottomNavigationBar(
+        currentIndex: 0,
+        selectedItemColor: Colors.pink.shade300,
+        unselectedItemColor: Colors.blue.shade100,
+        unselectedFontSize: 16,
+        selectedFontSize: 22,
+        showSelectedLabels: true,
+        unselectedIconTheme: IconThemeData(
+          color: Colors.amber.shade400
+        ),
+        onTap: (int selectedIndex){} ,
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label:"Home"),
+          BottomNavigationBarItem(icon: Icon(Icons.settings), label: "Settings"),
+        ],
+      ),*/
+      bottomNavigationBar: NavigationBar(
+        selectedIndex: 1,
+        onDestinationSelected: (int index){},
+        indicatorColor: Colors.pink,
+        //surfaceTintColor: Colors.blue,
+        labelBehavior: NavigationDestinationLabelBehavior.onlyShowSelected,
+        shadowColor: Colors.grey.shade400,
+        destinations: [
+          NavigationDestination(icon: Icon(Icons.home), label:"Home"),
+          NavigationDestination(icon: Icon(Icons.settings), label: "Settings"),
+        ],
+      ),
     );
   }
 
